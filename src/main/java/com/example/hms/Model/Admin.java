25b55616c534fdbe50e34071d26813f3f7ffd1e5
S-Nivedita <Nivedita.S2@cognizant.com>
2026-04-17 09:15:14 +0530

    Update Admin.java
    Added admin model

--- src/main/java/com/example/hms/Model/Admin.java
+++ src/main/java/com/example/hms/Model/Admin.java
@@ -1,4 +1,10 @@
 package com.example.hms.Model;
 
 public class Admin {
+   @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+    private LocalDateTime updationDate;
 }
